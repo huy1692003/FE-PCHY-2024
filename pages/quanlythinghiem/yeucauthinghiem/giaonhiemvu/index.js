abe4e869d457c32844353b80138d534bc8ce2d2f
huy169203 <daohuy1692003@gmail.com>
2024-11-29 14:26:44 +0700

    create useThongTinYCTN

--- pages/quanlythinghiem/yeucauthinghiem/giaonhiemvu/index.js
+++ pages/quanlythinghiem/yeucauthinghiem/giaonhiemvu/index.js
@@ -5,29 +5,9 @@ import FillThongTinYCTN from "../../../../utils/Components/FilterYCTN/FillThongT
 import QLTN_YCTNService from "../../../../services/quanlythinghiem/QLTN_YCTNService";
 import { useRouter } from "next/router";
 import ThongTinYCTN from "../../../../utils/Components/ListFieldYCTN/ThongTinYCTN";
+import useThongTinYCTN from "../../../../hooks/useThongTinYCTN";
 const GiaoNhiemVu = () => {
-    const [ma_yctn, setMaYCTN] = useState(null);
-    const [thongTinYCTN, setThongTinYCTN] = useState(null);
-    const router = useRouter();
-   
-
-    useEffect(() => {
-        if (router.query.code) {
-            setMaYCTN(router.query.code);
-        }
-    }, [router.query.code]);
-
-    useEffect(() => {
-        if (ma_yctn) {
-            getThongTinYCTN_byMaYCTN();
-        }
-    }, [ma_yctn]);
-
-    const getThongTinYCTN_byMaYCTN = async () => {
-        let res = await QLTN_YCTNService.get_QLTN_YCTN_ByMAYCTN(ma_yctn);
-        setThongTinYCTN(res);
-    }
-    
+    const {ma_yctn,thongTinYCTN}=useThongTinYCTN();
 
     return (
         <>
@@ -36,7 +16,7 @@ const GiaoNhiemVu = () => {
             </Head>
             <Panel header={<h3 className="text-xl font-bold">Giao nhiệm vụ thí nghiệm</h3>} className="mt-3">
                 <FillThongTinYCTN Element= {thongTinYCTN ? <ThongTinYCTN loai_yctn={thongTinYCTN.loai_yctn_model} formData={thongTinYCTN}/> : <></>}/>
-               
+                
 
             </Panel>
         </>
