97d6f4548ea7ef83775834019eee8b80f45e23b9
VinhVu2003 <vuv28104@gmail.com>
2024-12-06 14:19:30 +0700

    Chỉnh sửa chức năng nhập khối lượng và dịch vụ QLTN

--- pages/quanlythinghiem/yeucauthinghiem/nhapkhoiluongthuchien/index.js
+++ pages/quanlythinghiem/yeucauthinghiem/nhapkhoiluongthuchien/index.js
@@ -11,19 +11,16 @@ import FillThongTinYCTN from "../../../../utils/Components/FilterYCTN/FillThongT
 import ThongTinYCTN from "../../../../utils/Components/ListFieldYCTN/ThongTinYCTN";
 import { QLTN_THIET_BI_YCTN_Service } from "../../../../services/quanlythinghiem/QLTN_THIET_BI_YCTN_Service";
 import useThongTinYCTN from "../../../../hooks/useThongTinYCTN";
+import LichSuNhapKhoiLuong from "./LichSuNhapKhoiLuong";
 const NhapKhoiLuongThucHien = () => {
-
-//   const [MaYCTN, setMaYCTN] = useState("YCTN.HD-334");
-  const {ma_yctn,thongTinYCTN}=useThongTinYCTN();
-
-   
+  //   const [MaYCTN, setMaYCTN] = useState("YCTN.HD-334");
+  const { ma_yctn, thongTinYCTN } = useThongTinYCTN();
 
   //   const router = useRouter();
   //   useEffect(() => {
   //     setMaYCTN(router.query.code||"YCTN-666");
   //   }, [router.query.code]);
   //   console.log("MaYCTN:", MaYCTN);
-
 
   const [arrThietbi, setArrThietbi] = useState([]);
   const [showDialog, setShowDialog] = useState(false);
@@ -34,20 +31,23 @@ const NhapKhoiLuongThucHien = () => {
 
   const user = JSON.parse(sessionStorage.getItem("user"))?.ten_dang_nhap || "";
 
-  
+  const [isCurrent, setIsCurrent] = useState();
+
   const LoatArrThietBi = async () => {
     setArrThietbi(arrThietbi);
-  }
+  };
 
+  useEffect(() => {
+    setIsCurrent(thongTinYCTN?.crr_step);
+  }, [thongTinYCTN]);
   const headerList = (options) => {
     const className = `${options.className} flex flex-wrap justify-content-between align-items-center`;
 
     return (
       <>
         <div className={className + " mt-4"}>
-          <span className="font-bold text-lg">Danh sách thiết bị</span>
+          <span className="font-bold text-base">Danh sách thiết bị</span>
           <div className="flex flex-wrap gap-2">
-        
             <Button
               label="Thêm mới"
               style={{ backgroundColor: "#1445a7" }}
@@ -73,91 +73,106 @@ const NhapKhoiLuongThucHien = () => {
   const ThemMoiThietBi_YCTN = async () => {
     if (!ma_yctn) {
       toast.current.show({
-        severity: 'error',
-        summary: 'Lỗi',
-        detail: 'Vui lòng chọn một yêu cầu thí nghiệm',
-        life: 3000
+        severity: "error",
+        summary: "Lỗi",
+        detail: "Vui lòng chọn một yêu cầu thí nghiệm",
+        life: 3000,
       });
       return;
     }
     // console.log("ma_yctn", ma_yctn);
-    else{
-        const thietBiData = arrThietbi.map(item => ({
+    else {
+      const thietBiData = arrThietbi.map((item) => ({
         //   id: item.id?.toString() || "",
-          ma_tbtn: `${ma_yctn}-${item.id}`,
-          ma_yctn: ma_yctn,
-          ten_thiet_bi: item.ten_thiet_bi || "",
-          ma_loai_tb: item.ma_loai_tb || "",
-          so_luong: item.so_luong || 0,
-          trang_thai: 0, // thiết bị mới thêm
+        ma_tbtn: `${ma_yctn}-${item.id}`,
+        ma_yctn: ma_yctn,
+        ten_thiet_bi: item.ten_thiet_bi || "",
+        ma_loai_tb: item.ma_loai_tb || "",
+        so_luong: item.so_luong || 0,
+        trang_thai: 0, // thiết bị mới thêm
         //   ngay_tao: new Date().toISOString(),
-          nguoi_tao: user,
-          ten_loai_thiet_bi: item.ten_loai_thiet_bi || ""
-        }));
-        console.log("dsthietBiData trc lưu", thietBiData);
-        const res = await QLTN_THIET_BI_YCTN_Service.create_Multiple_QLTN_THIET_BI_YCTN(thietBiData);
-        toast.current.show({
-            severity: 'success',
-            summary: 'Thành công',
-            detail: 'Thêm mới thiết bị thành công',
-            life: 3000
-          });
-        // setArrThietbi([]);
-
+        nguoi_tao: user,
+        ten_loai_thiet_bi: item.ten_loai_thiet_bi || "",
+      }));
+      console.log("dsthietBiData trc lưu", thietBiData);
+      const res =
+        await QLTN_THIET_BI_YCTN_Service.create_Multiple_QLTN_THIET_BI_YCTN(
+          thietBiData
+        );
+      toast.current.show({
+        severity: "success",
+        summary: "Thành công",
+        detail: "Thêm mới thiết bị thành công",
+        life: 3000,
+      });
+      // setArrThietbi([]);
     }
-
-  }
+  };
 
   return (
     <div className="border-round-3xl bg-white p-4">
-        <Panel header="Nhập khối lượng thực hiện" className="mt-3">
+      <Panel header="Nhập khối lượng thực hiện" className="mt-3">
+        <FillThongTinYCTN
+          Element={
+            thongTinYCTN ? (
+              <ThongTinYCTN
+                loai_yctn={thongTinYCTN.loai_yctn_model}
+                formData={thongTinYCTN}
+              />
+            ) : (
+              <></>
+            )
+          }
+        />
 
-            <FillThongTinYCTN Element= {thongTinYCTN ? <ThongTinYCTN loai_yctn={thongTinYCTN.loai_yctn_model} formData={thongTinYCTN}/> : <></>}/>
-            
-            <Panel headerTemplate={headerList}>
-                <TableNhapKhoiLuong
-                arrThietbi={arrThietbi}
-                setArrThietbi={setArrThietbi}
-                setShowDialog={setShowDialog}
-                setIsAdd={setIsAdd}
-                setFormData={setFormData}
+        {thongTinYCTN && isCurrent === 3 && (
+          <Panel headerTemplate={headerList}>
+            <TableNhapKhoiLuong
+              arrThietbi={arrThietbi}
+              setArrThietbi={setArrThietbi}
+              setShowDialog={setShowDialog}
+              setIsAdd={setIsAdd}
+              setFormData={setFormData}
+            />
+            {/* start_button lưu, hủy */}
+            {arrThietbi.length > 0 && (
+              <div className="flex justify-content-end gap-2 mt-3">
+                <Button
+                  label="Hủy"
+                  severity="secondary"
+                  outlined
+                  onClick={() => {
+                    setArrThietbi([]);
+                  }}
                 />
-
-                {/* start_button lưu, hủy */}
-                {arrThietbi.length > 0 && (
-                <div className="flex justify-content-end gap-2 mt-3">
-                    <Button
-                    label="Lưu"
-                    style={{ backgroundColor: "#1445a7" }}
-                    onClick={() => {
-                        ThemMoiThietBi_YCTN();
-                    }}
-                    />
-                    <Button
-                    label="Hủy" 
-                    severity="secondary"
-                    outlined
-                    onClick={() => {
-                        setArrThietbi([]);
-                    }}
-                    />
-                    <Button
-                    label="Lưu và chuyển bước"
-                    style={{ backgroundColor: "#1445a7" }}
-                    onClick={() => {
-                        ThemMoiThietBi_YCTN();
-                    }}
-                    />
-                </div>
-                )}
-                {/* end_button lưu, hủy */}
-
-            </Panel>
-
+                <Button
+                  label="Lưu"
+                  style={{ backgroundColor: "#1445a7" }}
+                  onClick={() => {
+                    ThemMoiThietBi_YCTN();
+                    setIsCurrent(4);
+                  }}
+                />
+                
+                <Button
+                  label="Lưu và chuyển bước"
+                  style={{ backgroundColor: "#1445a7" }}
+                  onClick={() => {
+                    ThemMoiThietBi_YCTN();
+                  }}
+                />
+              </div>
+            )}
+            {/* end_button lưu, hủy */}
+          </Panel>
+        )}
+        
+        {thongTinYCTN && isCurrent > 3 && (
+        <Panel header="Lịch sử nhập khối lượng">
+          <LichSuNhapKhoiLuong MA_YCTN={ma_yctn} />
         </Panel>
-
-
-
+        )}
+      </Panel>
 
       <DialogForm
         show={showDialog}
